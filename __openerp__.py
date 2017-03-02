b6f90d79086770f6cfa32ffbfd13028a1b65389c
MohssinBouktaib <bouktaib.mohssin@gmail.com>
2017-03-02 11:01:56 +0000

    constraint tutorial odooforbeginners

---
+++ __openerp__.py
@@ -0,0 +1,20 @@
+{
+    'name': 'School Manager',
+    'version': '9.0.1.0.0',
+    'license': "AGPL-3",
+    'category': 'Education',
+    'description': """
+    This is tutorial addons
+
+    """,
+    'author': 'OdooForBeginners',
+    'website': 'https://odooforbeginnersblog.wordpress.com',
+    'depends': [],
+    'data': [
+        'views/marks_view.xml',
+        'views/menus.xml'],
+    'css': [],
+    'test': [],
+    'installable': True,
+    'active': False
+}
